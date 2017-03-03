e9b8c1722f7a2ebaa34e6766b713b232f6537eaf
Zhiyuan Cui <zhiyuan.cui@usmb30356.local>
2017-03-03 16:26:24 -0500

    Initial Commit

---
+++ socialClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialClone
+//
+//  Created by Zhiyuan Cui on 3/3/17.
+//  Copyright © 2017 Zhiyuan Cui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
