7bcc0047e264506e91a67152c18fd5eda0bb1bec
Zhiyuan Cui <zhiyuan.cui@usmb30356.ert.com>
2017-03-06 17:27:37 -0500

    Basic Sign in layout

--- socialClone/SignInVC.swift
+++ socialClone/SignInVC.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var signUpLabel: UILabel!
+    @IBOutlet weak var forgetPwdLabel: UILabel!
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginWithFB: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        signUpLabel.attributedText = getSignUpAttributedText()
+        forgetPwdLabel.attributedText = getForgetPwdArributedText()
+        
+        // Setup gesture listener
+        let signUpTap = UITapGestureRecognizer(target: self, action: #selector(self.signUpTapFunction))
+        signUpLabel.isUserInteractionEnabled = true
+        signUpLabel.addGestureRecognizer(signUpTap)
+        
+        let forgetPwdTap = UITapGestureRecognizer(target: self, action: #selector(self.forgetPwdTapFunction))
+        forgetPwdLabel.isUserInteractionEnabled = true
+        forgetPwdLabel.addGestureRecognizer(forgetPwdTap)
+        
+        let letSignInTap = UITapGestureRecognizer(target: self, action: #selector(self.SignInWithFBTapFunction))
+        loginWithFB.isUserInteractionEnabled = true
+        loginWithFB.addGestureRecognizer(letSignInTap)
+
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,129 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func LoginWithPwd(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func SigninFromFB(_ sender: Any) {
+        
+    }
+    
+    //Create an attributeText for signup Label
+    func getSignUpAttributedText() -> NSMutableAttributedString{
+        let myMutableString = NSMutableAttributedString(
+            string: "Don't have an account? Sign up")
+        
+        myMutableString.addAttribute(
+            NSForegroundColorAttributeName,
+            value: UIColor.init(hex: "1565C0"),
+            range: NSRange(
+                location:23,
+                length:7))
+        
+        return myMutableString
+    }
+    
+    //Create an attributeText for Forget Password Label Label
+    func getForgetPwdArributedText() -> NSMutableAttributedString{
+        let myMutableString = NSMutableAttributedString(
+            string: "Forgot your login details? Get help signing in.")
+        
+        myMutableString.addAttribute(
+            NSForegroundColorAttributeName,
+            value: UIColor.init(hex: "1565C0"),
+            range: NSRange(
+                location:27,
+                length:20))
+        
+        return myMutableString
+    }
+    
+    func signUpTapFunction(sender: UITapGestureRecognizer){
+        let tapLocation = sender.location(in: signUpLabel)
+        let index = signUpLabel.indexOfAttributedTextCharacterAtPoint(point: tapLocation)
+        if( index > 22 ){
+            //Bring up sing up page
+            print("sign up")
+        }
+    }
 
+    func forgetPwdTapFunction(sender: UITapGestureRecognizer){
+        let tapLocation = sender.location(in: forgetPwdLabel)
+        let index = forgetPwdLabel.indexOfAttributedTextCharacterAtPoint(point: tapLocation)
+        if( index > 27 ){
+            //Bring up forget password page
+            print("forget password")
+        }
+    }
+    
+    func SignInWithFBTapFunction(sender: UITapGestureRecognizer){
+        
+    }
+
+    
+}
+
+
+///////////////////////////////////////////////////////////////////
+///                         EXTENSIONS                          ///
+///////////////////////////////////////////////////////////////////
+extension UIColor{
+    //Init UIColor from hex string
+    convenience init(hex: String){
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0;
+        
+        var rgbValue: UInt64 = 0;
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+}
+
+extension UILabel {
+    ///Find the index of character (in the attributedText) at point
+    func indexOfAttributedTextCharacterAtPoint(point: CGPoint) -> Int {
+        assert(self.attributedText != nil, "This method is developed for attributed string")
+        
+        let attributedText = NSMutableAttributedString(attributedString: self.attributedText!)
+        attributedText.addAttributes([NSFontAttributeName: self.font], range: NSMakeRange(0, (self.attributedText?.string.characters.count)!))
+        
+        
+        let textStorage = NSTextStorage(attributedString: attributedText)
+        
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize(width: self.frame.width, height: (self.frame.height-100)))
+        
+        //Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+        
+        
+        //Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.maximumNumberOfLines = self.numberOfLines
+        textContainer.lineBreakMode = self.lineBreakMode
+        textContainer.size = self.bounds.size
+        
+        print("Points: \(point)")
+        let alignedPoint = CGPoint.init(x: point.x-47, y: point.y-20)
+        print("alignedPoint: \(alignedPoint)")
+        let index = layoutManager.characterIndex(for: alignedPoint, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        print("Index: \(index)")
+        
+        return index
+    }
 }
 
